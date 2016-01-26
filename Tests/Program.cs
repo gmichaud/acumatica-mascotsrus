5b4b13536d44698ba1e932c046429458f5d554bf
gmichaud <gabriel@velixo.com>
2016-01-25 19:16:20 -0800

    Cleaned test script for live demo

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -10,28 +10,7 @@ namespace Acumatica.RentalPro
     {
         public override void Execute()
         {
-            PxLogin.LogIn("admin", "admin");
-            var toolsForm = new RentalItemManagement();
-            toolsForm.OpenScreen();
-
-            toolsForm.Insert();
-            toolsForm.Summary.RentalItemCD.Type("ABC");
-            toolsForm.Summary.Description.Type("My new costume");
-            toolsForm.Summary.SerialNumber.Type("XYZ"); //System should complain that serial number doesn't start with MRU-.
-
-            try
-            {
-                toolsForm.Save();
-                Log.Error("Rental item should not have saved, serial number is invalid");
-            }
-            catch(Exception e)
-            {
-                Log.Information(e.Message);
-            }
-
-            toolsForm.Summary.SerialNumber.Type("MRU-1234");
-            toolsForm.Save();
-            Log.Information("Test completed!");
+            
         }
     }
 }
